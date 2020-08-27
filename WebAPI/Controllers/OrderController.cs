fcc5ea81b1d53ba71e6ad9efa0c62fa8bfdf3ba3
HoangThuongBoz <hoangthuongboz@gmail.com>
2020-08-27 17:11:12 +0700

    first commit

---
+++ WebAPI/Controllers/OrderController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DTO;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : BaseController
+    {
+        // GET: api/<OrderController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<OrderController>/5
+        [HttpGet("{id}")]
+        public List<Orders> Get(long id)
+        {
+            return app.Order.GetOrders(id);
+        }
+
+        // POST api/<OrderController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<OrderController>/5
+        [HttpPut("{id}")]
+        public int Put(Orders obj)
+        {
+            return app.Order.Pay(obj);
+        }
+
+        // DELETE api/<OrderController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebClient/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using DTO;
+using Microsoft.AspNetCore.Mvc;
+using WebClient.Filters;
+using WebClient.Models;
+
+namespace WebClient.Controllers
+{
+    
+    public class OrderController : BaseController
+    {
+        AppService app = new AppService();
+        public IActionResult Index()
+        {
+            long id = long.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            List<Orders> list = app.Order.GetOrders(id);
+            return View(list);
+        }
+        public IActionResult Pay(Orders obj)
+        {
+            long id = long.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            obj.AccountId = id;
+            obj.Id = CartId;
+            obj.Status = "P";
+
+            if (app.Order.Pay(obj) > 0)
+            {
+                OrderItems orderItems = new OrderItems();
+                Orders orders = new Orders();
+                return RedirectToAction("Index");
+            }
+            return Redirect("/");
+        }
+    }
+}
